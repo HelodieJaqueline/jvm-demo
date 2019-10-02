ea19f618c5eff2737533d856378f18ba4aaa38bd
zhangrui <zhangrui@ysten.com>
2019-10-02 08:16:33 +0800

    Initial commit

---
+++ src/main/java/com/example/jvm/chaper2/EdenAllocator.java
@@ -0,0 +1,9 @@
+package com.example.jvm.chaper2;
+
+public class EdenAllocator {
+
+    public static void main(String[] args) {
+        byte[] data = new byte[20 * 1024 * 1024];
+
+    }
+}
---
+++ src/main/java/com/example/jvm/chaper2/SpaceGuarantee.java
@@ -0,0 +1,14 @@
+package com.example.jvm.chaper2;
+
+public class SpaceGuarantee {
+
+    public static void main(String[] args) {
+        byte[] d1 = new byte[2 * 1024 * 1024];
+        byte[] d2 = new byte[2 * 1024 * 1024];
+        byte[] d3 = new byte[2 * 1024 * 1024];
+        byte[] d4 = new byte[4 * 1024 * 1024];
+
+        System.gc();
+
+    }
+}
